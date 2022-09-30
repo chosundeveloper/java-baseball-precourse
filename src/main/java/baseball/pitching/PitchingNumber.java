d139eaa2effa43f818a7a08b8fdb61a998934c3f
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-09-30 23:57:42 +0900

    feat(PitchingNumber.java) : add equals & hashcode

--- src/main/java/baseball/pitching/PitchingNumber.java
+++ src/main/java/baseball/pitching/PitchingNumber.java
@@ -1,34 +1,47 @@
 package baseball.pitching;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class PitchingNumber {
 
-    public static final int MAX_PITCHING_NUMBER = 9;
-    public static final int MIN_PITCHING_NUMBER = 1;
-    private final int number;
+    public static final BigDecimal MAX_PITCHING_NUMBER = BigDecimal.valueOf(9);
+    public static final BigDecimal MIN_PITCHING_NUMBER = BigDecimal.valueOf(1);
+    private final BigDecimal number;
 
-    public PitchingNumber(int number) {
+    public PitchingNumber(BigDecimal number) {
         validate(number);
         this.number = number;
     }
 
-    private void validate(int number) {
+    private void validate(BigDecimal number) {
         validateMaxPitchingNumber(number);
         validateMinPitchingNumber(number);
     }
 
-    private static void validateMaxPitchingNumber(int number) {
-        if (MAX_PITCHING_NUMBER < number) {
+    private static void validateMaxPitchingNumber(BigDecimal number) {
+        if (number.compareTo(MAX_PITCHING_NUMBER) > 0) {
             throw new IllegalArgumentException("9보다 클 수 없습니다.");
         }
     }
 
-    private void validateMinPitchingNumber(int number) {
-        if (number < MIN_PITCHING_NUMBER) {
+    private void validateMinPitchingNumber(BigDecimal number) {
+        if (number.compareTo(MIN_PITCHING_NUMBER) < 0) {
             throw new IllegalArgumentException("1보다 작을 수 없습니다.");
         }
     }
 
-    public int getNumber() {
-        return number;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PitchingNumber that = (PitchingNumber) o;
+        return number.equals(that.number);
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
 }
