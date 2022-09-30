89a76e5c62ba5c2cc5c48466bb8c554b78025186
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-09-30 23:58:53 +0900

    feat(PitchingLocation.java) : 투구위치VO add equals & hashcode

--- src/main/java/baseball/pitching/PitchingLocation.java
+++ src/main/java/baseball/pitching/PitchingLocation.java
@@ -1,34 +1,47 @@
 package baseball.pitching;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class PitchingLocation {
 
-    public static final int MAX_PITCHING_LOCATION = 3;
-    public static final int MIN_PITCHING_LOCATION = 1;
-    private final int location;
+    public static final BigDecimal MAX_PITCHING_LOCATION = BigDecimal.valueOf(3);
+    public static final BigDecimal MIN_PITCHING_LOCATION = BigDecimal.valueOf(1);
+    private final BigDecimal location;
 
-    public PitchingLocation(int location) {
+    public PitchingLocation(BigDecimal location) {
         validate(location);
         this.location = location;
     }
 
-    private void validate(int location) {
+    private void validate(BigDecimal location) {
         validateMaxPitchingLocation(location);
         validateMinPitchingLocation(location);
     }
 
-    private static void validateMaxPitchingLocation(int location) {
-        if (location > MAX_PITCHING_LOCATION) {
+    private static void validateMaxPitchingLocation(BigDecimal location) {
+        if (location.compareTo(MAX_PITCHING_LOCATION) > 0) {
             throw new IllegalArgumentException("3보다 클 수 없습니다.");
         }
     }
 
-    private void validateMinPitchingLocation(int location) {
-        if (location < MIN_PITCHING_LOCATION) {
+    private void validateMinPitchingLocation(BigDecimal location) {
+        if (location.compareTo(MIN_PITCHING_LOCATION) < 0) {
             throw new IllegalArgumentException("1보다 작을 수 없습니다.");
         }
     }
 
-    public int getLocation() {
-        return location;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PitchingLocation that = (PitchingLocation) o;
+        return location.equals(that.location);
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(location);
+    }
+
 }
